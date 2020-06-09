9183dfbefad234bd62411a0bdad30cb3609f07c4
narudb <benjam.d@gmail.com>
2020-06-09 11:42:31 +0200

    quete redux with react

--- src/App.js
+++ src/App.js
@@ -3,20 +3,7 @@ import "./App.css";
 import CounterContainer from "./CounterContainer";
 
 class App extends Component {
-  /*constructor(props) {
-    super(props);
-    this.state = {
-      light: "on",
-    };
-    this.onSwitch = this.onSwitch.bind(this);
-  }
-
-  onSwitch() {
-    const { light } = this.state;
-    this.setState({ light: light === "on" ? "off" : "on" });
-  }*/
   render() {
-    //const { light } = this.state;
     return (
       <div className="App">
         <CounterContainer />
